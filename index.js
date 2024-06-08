4c78a2c87c3c651442086694f97212166aa6b4c3
userDymon <dimagrabovskuy@gmail.com>
2024-06-08 14:51:04 +0300

    backend v0.1

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+.connect('mongodb+srv://admin:admin@atlascluster.2axeb9c.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster')
+.then(() => console.log("DB OK"))
+.catch((err) => console.log('DB error', err));
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullname: "Дмитро Дмитро"
+    }, 'secret123');
+
+    res.json({
+        success: true,
+        token,
+    });
 });
 
 app.listen(4444, (err) => {
