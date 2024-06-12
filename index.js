2f6350c239643e01ee60b12ca4a247802496ec2c
userDymon <dimagrabovskuy@gmail.com>
2024-06-12 22:45:33 +0300

    backend v0.2.2

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import { validationResult } from "express-validator";
 
 import { registerValidation } from "./validations/auth.js";
 
-import UserModel from "./models/User.js";
+import checkAuth from "./utils/checkAuth.js";
+
+import * as UserController from "./controllers/UserController.js";
 
 mongoose
     .connect('mongodb+srv://admin:admin@atlascluster.2axeb9c.mongodb.net/blog?retryWrites=true&w=majority&appName=AtlasCluster')
@@ -17,49 +16,16 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-    try{
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array());
-        }
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
+app.post('/auth/login', UserController.login);
 
-        const doc = new UserModel({
-            fullName: req.body.fullName,
-            email: req.body.email,
-            avatarUrl: req.body.avatarUrl,
-            passwordHash : hash,
-        });
+app.post('/auth/register', registerValidation, UserController.register);
 
-        const user = await doc.save();
-
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123',
-            {
-                expiresIn: '30d',
-            },
-        );
-        
-        const { passwordHash, ...userData } = user._doc;
-
-        res.json({
-            ...userData,
-            token,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Failed to register',
-        });
-    }
-});
+app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    
     console.log("Server started on port 4444");
 });
